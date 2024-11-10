6cf502497388539b1fc61cb0c37863c18044c569
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-11-10 20:17:37 +0600

    moved the logic

--- src/components/home-page-components/introductioncard/LeftCard.jsx
+++ src/components/home-page-components/introductioncard/LeftCard.jsx
@@ -9,44 +9,16 @@ import {
   Github,
 } from './StyleIntroductionCard';
 import LinkedinComponent from './Linkedin';
-import { TitleModalLogic, TitleModalLogicWhoIam } from './modals/LogicModals';
+import {
+  MainTextLogic,
+  TitleModalLogic,
+  TitleModalLogicWhoIam,
+} from './modals/LogicModals';
 import TitleModal from './modals/TitleModal';
 import SubTitleModalWhoIam from './modals/SubTitleModalWhoIam';
 import MainTextModals from './modals/MainTextModals';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  mainTestGet,
-  maintTextPost,
-} from '../../../store/slices/edit-profile-slice/editProfile.thunk';
 
 const LeftCard = () => {
-  const dispatch = useDispatch();
-  const { mainText } = useSelector((state) => state.editProfile);
-  const [openMainText, setOpenMainText] = useState(false);
-  const [inputValueMainText, setInputValueMainText] = useState('');
-  const [inputValueMainTextSecond, setInputValueMainTextSecond] = useState('');
-  const [inputValueMainTextThird, setInputValueMainTextThird] = useState('');
-  const [inputValueMainTextFour, setInputValueMainTextFour] = useState('');
-  const [inputValueMainTextFive, setInputValueMainTextFive] = useState('');
-  const [inputValueMainTextSix, setInputValueMainTextSix] = useState('');
-  const [inputValueMainTextSeven, setInputValueMainTextSeven] = useState('');
-  const [inputValueMainTextEight, setInputValueMainTextEight] = useState('');
-  const [displayMaintext, setDisplayMainText] = useState('Как опытный');
-  const [displayMaintextSecond, setDisplayMainTextSecond] = useState(
-    'Frontend-разработчик'
-  );
-  const [displayMaintextThird, setDisplayMainTextThird] =
-    useState(`, я посвящаю себя воплощению идей в инновационные веб-приложения.
-          Ознакомьтесь с моими недавними`);
-  const [displayMaintextFour, setDisplayMainTextFour] = useState('проектами');
-  const [displayMaintextFive, setDisplayMainTextFive] = useState('и');
-  const [displayMaintextSix, setDisplayMainTextSix] = useState('статьями');
-  const [displayMaintextSeven, setDisplayMainTextSeven] = useState(
-    ', которые демонстрируют мои навыки в Front-End разработке.'
-  );
-  const [displayMaintextEight, setDisplayMainTextEight] = useState('📍');
-
   const {
     isAuth,
     open,
@@ -75,128 +47,40 @@ const LeftCard = () => {
     inputWhoIamSecond,
   } = TitleModalLogicWhoIam();
 
+  const {
+    openMainText,
+    handleOpenMainText,
+    handleCloseMainText,
+    handleInputValueMainText,
+    handleInputValueMainTextSecond,
+    handleInputValueMainTextThird,
+    handleInputValueMainTextFour,
+    handleInputValueMainTextFive,
+    handleInputValueMainTextSix,
+    handleInputValueMainTextSeven,
+    handleInputValueMainTextEight,
+    handleSubmitMainText,
+    displayMaintext,
+    displayMaintextSecond,
+    displayMaintextThird,
+    displayMaintextFour,
+    displayMaintextFive,
+    displayMaintextSix,
+    displayMaintextSeven,
+    displayMaintextEight,
+    inputValueMainText,
+    inputValueMainTextSecond,
+    inputValueMainTextThird,
+    inputValueMainTextFour,
+    inputValueMainTextFive,
+    inputValueMainTextSix,
+    inputValueMainTextSeven,
+    inputValueMainTextEight,
+  } = MainTextLogic();
+
   const handleGithub = () => {
     window.open('https://github.com/adiletzainidinov', '_blank');
   };
-
-  const handleOpenMainText = () => {
-    setOpenMainText(true);
-  };
-
-  const handleCloseMainText = () => {
-    setOpenMainText(false);
-  };
-
-  const handleInputValueMainText = (e) => {
-    setInputValueMainText(e.target.value);
-  };
-
-  const handleInputValueMainTextSecond = (e) => {
-    setInputValueMainTextSecond(e.target.value);
-  };
-
-  const handleInputValueMainTextThird = (e) => {
-    setInputValueMainTextThird(e.target.value);
-  };
-
-  const handleInputValueMainTextFour = (e) => {
-    setInputValueMainTextFour(e.target.value);
-  };
-
-  const handleInputValueMainTextFive = (e) => {
-    setInputValueMainTextFive(e.target.value);
-  };
-
-  const handleInputValueMainTextSix = (e) => {
-    setInputValueMainTextSix(e.target.value);
-  };
-
-  const handleInputValueMainTextSeven = (e) => {
-    setInputValueMainTextSeven(e.target.value);
-  };
-
-  const handleInputValueMainTextEight = (e) => {
-    setInputValueMainTextEight(e.target.value);
-  };
-
-  useEffect(() => {
-    dispatch(mainTestGet());
-  }, [dispatch]);
-
-
-  useEffect(() => {
-    if (mainText?.length > 0) {
-      const lastItem = mainText[mainText?.length - 1];
-      setDisplayMainText(lastItem.first || 'Как опытный');
-      setDisplayMainTextSecond(lastItem.second || 'Frontend-разработчик');
-      setDisplayMainTextThird(
-        lastItem.third ||
-          `, я посвящаю себя воплощению идей в инновационные веб-приложения.
-          Ознакомьтесь с моими недавними`
-      );
-      setDisplayMainTextFour(lastItem.four || `проектами`);
-      setDisplayMainTextFive(lastItem.five || `и`)
-      setDisplayMainTextSix(lastItem.six || 'статьями')
-      setDisplayMainTextSeven(lastItem.seven || ', которые демонстрируют мои навыки в Front-End разработке.')
-      setDisplayMainTextEight(lastItem.eight || '📍')
-    }
-  }, [mainText]);
-  console.log(mainText)
-
-  const handleSubmitMainText = () => {
-    if (
-      inputValueMainText?.length +
-        inputValueMainTextSecond?.length +
-        inputValueMainTextThird?.length +
-        inputValueMainTextFour?.length +
-        inputValueMainTextFive?.length +
-        inputValueMainTextSix?.length +
-        inputValueMainTextSeven?.length +
-        inputValueMainTextEight?.length ===
-      211
-    ) {
-      const newDataMainText = {
-        first: inputValueMainText,
-        second: inputValueMainTextSecond,
-        third: inputValueMainTextThird,
-        four: inputValueMainTextFour,
-        five: inputValueMainTextFive,
-        six: inputValueMainTextSix,
-        seven: inputValueMainTextSeven,
-        eight: inputValueMainTextEight,
-      };
-      dispatch(maintTextPost(newDataMainText));
-      setInputValueMainText('');
-      setInputValueMainTextSecond('');
-      setInputValueMainTextThird('');
-      setInputValueMainTextFour('');
-      setInputValueMainTextFive('');
-      setInputValueMainTextSix('');
-      setInputValueMainTextSeven('');
-      setInputValueMainTextEight('');
-      setDisplayMainText(inputValueMainText);
-      setDisplayMainTextSecond(inputValueMainTextSecond);
-      setDisplayMainTextThird(inputValueMainTextThird);
-      setDisplayMainTextFour(inputValueMainTextFour);
-      setDisplayMainTextFive(inputValueMainTextFive);
-      setDisplayMainTextSix(inputValueMainTextSix);
-      setDisplayMainTextSeven(inputValueMainTextSeven);
-      setDisplayMainTextEight(inputValueMainTextEight);
-      setOpenMainText(false);
-    }
-  };
-  console.log(
-    "Current length:",
-    displayMaintext.length +
-    displayMaintextSecond.length +
-    displayMaintextThird.length +
-    displayMaintextFour.length +
-    displayMaintextFive.length +
-    displayMaintextSix.length +
-    displayMaintextSeven.length +
-    displayMaintextEight.length
-  );
-  
 
   return (
     <>
@@ -299,4 +183,3 @@ const LeftCard = () => {
 };
 
 export default LeftCard;
-
--- src/components/home-page-components/introductioncard/modals/LogicModals.js
+++ src/components/home-page-components/introductioncard/modals/LogicModals.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   aboutMeGet,
   aboutMeThunk,
+  mainTestGet,
+  maintTextPost,
   whoIAmGet,
   whoIAmPost,
 } from '../../../../store/slices/edit-profile-slice/editProfile.thunk';
@@ -89,8 +91,9 @@ export const TitleModalLogicWhoIam = () => {
   const [inputWhoIamSecond, setInputWhoIamSecond] = useState('');
   const [errorWhoIam, setErrorWhoIam] = useState('');
   const [displayTextWhoIam, setDisplayTextWhoIam] = useState('Я');
-  const [displayTextWhoIamSecond, setDisplayTextWhoIamSecond] = useState('Frontend-разработчик');
-
+  const [displayTextWhoIamSecond, setDisplayTextWhoIamSecond] = useState(
+    'Frontend-разработчик'
+  );
 
   useEffect(() => {
     dispatch(whoIAmGet());
@@ -99,7 +102,7 @@ export const TitleModalLogicWhoIam = () => {
   useEffect(() => {
     if (dataWhoIam.length > 0) {
       const lastItem = dataWhoIam[dataWhoIam.length - 1];
-      setDisplayTextWhoIam(lastItem.title || 'Я')
+      setDisplayTextWhoIam(lastItem.title || 'Я');
       setDisplayTextWhoIamSecond(lastItem.subTitle || 'Frontend-разработчик');
     }
   }, [dataWhoIam]);
@@ -107,8 +110,7 @@ export const TitleModalLogicWhoIam = () => {
   const handleOpenWhoIam = () => setOpenWhoIam(true);
   const handleCloseWhoIam = () => setOpenWhoIam(false);
 
-  const 
-  handleSubminWhoIam = () => {
+  const handleSubminWhoIam = () => {
     if (inputWhoIam.length + inputWhoIamSecond.length === 21) {
       const newData = { title: inputWhoIam, subTitle: inputWhoIamSecond };
       dispatch(whoIAmPost(newData));
@@ -125,16 +127,17 @@ export const TitleModalLogicWhoIam = () => {
 
   const handleInputValueWhoIam = (e, inputName) => {
     const value = e.target.value;
-  
+
     if (inputName === 'inputWhoIam') {
       setInputWhoIam(value);
     } else if (inputName === 'inputWhoIamSecond') {
       setInputWhoIamSecond(value);
     }
-  
+
     const newInputWhoIam = inputName === 'inputWhoIam' ? value : inputWhoIam;
-    const newInputWhoIamSecond = inputName === 'inputWhoIamSecond' ? value : inputWhoIamSecond;
-  
+    const newInputWhoIamSecond =
+      inputName === 'inputWhoIamSecond' ? value : inputWhoIamSecond;
+
     const totalLength = newInputWhoIam.length + newInputWhoIamSecond.length;
     setErrorWhoIam(
       totalLength < 21
@@ -144,9 +147,12 @@ export const TitleModalLogicWhoIam = () => {
         : 'Превышен лимит символов!'
     );
   };
-  
+
   const handleKeyPressWhoIam = (e) => {
-    if (e.key === 'Enter' && inputWhoIam.length + inputWhoIamSecond.length === 21) {
+    if (
+      e.key === 'Enter' &&
+      inputWhoIam.length + inputWhoIamSecond.length === 21
+    ) {
       handleSubminWhoIam();
     }
   };
@@ -163,9 +169,188 @@ export const TitleModalLogicWhoIam = () => {
     handleCloseWhoIam,
     handleInputValueWhoIam,
     handleKeyPressWhoIam,
-    handleSubminWhoIam
+    handleSubminWhoIam,
   };
 };
 
 // MainTextModal Logic
+
+export const MainTextLogic = () => {
+  const dispatch = useDispatch();
+  const { mainText } = useSelector((state) => state.editProfile);
+  const [openMainText, setOpenMainText] = useState(false);
+  const [inputValueMainText, setInputValueMainText] = useState('');
+  const [inputValueMainTextSecond, setInputValueMainTextSecond] = useState('');
+  const [inputValueMainTextThird, setInputValueMainTextThird] = useState('');
+  const [inputValueMainTextFour, setInputValueMainTextFour] = useState('');
+  const [inputValueMainTextFive, setInputValueMainTextFive] = useState('');
+  const [inputValueMainTextSix, setInputValueMainTextSix] = useState('');
+  const [inputValueMainTextSeven, setInputValueMainTextSeven] = useState('');
+  const [inputValueMainTextEight, setInputValueMainTextEight] = useState('');
+  const [displayMaintext, setDisplayMainText] = useState('Как опытный');
+  const [displayMaintextSecond, setDisplayMainTextSecond] = useState(
+    'Frontend-разработчик'
+  );
+  const [displayMaintextThird, setDisplayMainTextThird] =
+    useState(`, я посвящаю себя воплощению идей в инновационные веб-приложения.
+          Ознакомьтесь с моими недавними`);
+  const [displayMaintextFour, setDisplayMainTextFour] = useState('проектами');
+  const [displayMaintextFive, setDisplayMainTextFive] = useState('и');
+  const [displayMaintextSix, setDisplayMainTextSix] = useState('статьями');
+  const [displayMaintextSeven, setDisplayMainTextSeven] = useState(
+    ', которые демонстрируют мои навыки в Front-End разработке.'
+  );
+  const [displayMaintextEight, setDisplayMainTextEight] = useState('📍');
+
+  const handleOpenMainText = () => {
+    setOpenMainText(true);
+  };
+
+  const handleCloseMainText = () => {
+    setOpenMainText(false);
+  };
+
+  const handleInputValueMainText = (e) => {
+    setInputValueMainText(e.target.value);
+  };
+
+  const handleInputValueMainTextSecond = (e) => {
+    setInputValueMainTextSecond(e.target.value);
+  };
+
+  const handleInputValueMainTextThird = (e) => {
+    setInputValueMainTextThird(e.target.value);
+  };
+
+  const handleInputValueMainTextFour = (e) => {
+    setInputValueMainTextFour(e.target.value);
+  };
+
+  const handleInputValueMainTextFive = (e) => {
+    setInputValueMainTextFive(e.target.value);
+  };
+
+  const handleInputValueMainTextSix = (e) => {
+    setInputValueMainTextSix(e.target.value);
+  };
+
+  const handleInputValueMainTextSeven = (e) => {
+    setInputValueMainTextSeven(e.target.value);
+  };
+
+  const handleInputValueMainTextEight = (e) => {
+    setInputValueMainTextEight(e.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(mainTestGet());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (mainText?.length > 0) {
+      const lastItem = mainText[mainText?.length - 1];
+      setDisplayMainText(lastItem.first || 'Как опытный');
+      setDisplayMainTextSecond(lastItem.second || 'Frontend-разработчик');
+      setDisplayMainTextThird(
+        lastItem.third ||
+          `, я посвящаю себя воплощению идей в инновационные веб-приложения.
+          Ознакомьтесь с моими недавними`
+      );
+      setDisplayMainTextFour(lastItem.four || `проектами`);
+      setDisplayMainTextFive(lastItem.five || `и`);
+      setDisplayMainTextSix(lastItem.six || 'статьями');
+      setDisplayMainTextSeven(
+        lastItem.seven ||
+          ', которые демонстрируют мои навыки в Front-End разработке.'
+      );
+      setDisplayMainTextEight(lastItem.eight || '📍');
+    }
+  }, [mainText]);
+  console.log(mainText);
+
+  const handleSubmitMainText = () => {
+    if (
+      inputValueMainText?.length +
+        inputValueMainTextSecond?.length +
+        inputValueMainTextThird?.length +
+        inputValueMainTextFour?.length +
+        inputValueMainTextFive?.length +
+        inputValueMainTextSix?.length +
+        inputValueMainTextSeven?.length +
+        inputValueMainTextEight?.length ===
+      211
+    ) {
+      const newDataMainText = {
+        first: inputValueMainText,
+        second: inputValueMainTextSecond,
+        third: inputValueMainTextThird,
+        four: inputValueMainTextFour,
+        five: inputValueMainTextFive,
+        six: inputValueMainTextSix,
+        seven: inputValueMainTextSeven,
+        eight: inputValueMainTextEight,
+      };
+      dispatch(maintTextPost(newDataMainText));
+      setInputValueMainText('');
+      setInputValueMainTextSecond('');
+      setInputValueMainTextThird('');
+      setInputValueMainTextFour('');
+      setInputValueMainTextFive('');
+      setInputValueMainTextSix('');
+      setInputValueMainTextSeven('');
+      setInputValueMainTextEight('');
+      setDisplayMainText(inputValueMainText);
+      setDisplayMainTextSecond(inputValueMainTextSecond);
+      setDisplayMainTextThird(inputValueMainTextThird);
+      setDisplayMainTextFour(inputValueMainTextFour);
+      setDisplayMainTextFive(inputValueMainTextFive);
+      setDisplayMainTextSix(inputValueMainTextSix);
+      setDisplayMainTextSeven(inputValueMainTextSeven);
+      setDisplayMainTextEight(inputValueMainTextEight);
+      setOpenMainText(false);
+    }
+  };
+  console.log(
+    'Current length:',
+    displayMaintext.length +
+      displayMaintextSecond.length +
+      displayMaintextThird.length +
+      displayMaintextFour.length +
+      displayMaintextFive.length +
+      displayMaintextSix.length +
+      displayMaintextSeven.length +
+      displayMaintextEight.length
+  );
+
+  return {
+    openMainText,
+    handleOpenMainText,
+    handleCloseMainText,
+    handleInputValueMainText,
+    handleInputValueMainTextSecond,
+    handleInputValueMainTextThird,
+    handleInputValueMainTextFour,
+    handleInputValueMainTextFive,
+    handleInputValueMainTextSix,
+    handleInputValueMainTextSeven,
+    handleInputValueMainTextEight,
+    handleSubmitMainText,
+    displayMaintext,
+    displayMaintextSecond,
+    displayMaintextThird,
+    displayMaintextFour,
+    displayMaintextFive,
+    displayMaintextSix,
+    displayMaintextSeven,
+    displayMaintextEight,
+    inputValueMainText,
+    inputValueMainTextSecond,
+    inputValueMainTextThird,
+    inputValueMainTextFour,
+    inputValueMainTextFive,
+    inputValueMainTextSix,
+    inputValueMainTextSeven,
+    inputValueMainTextEight,
+  };
+};
 
